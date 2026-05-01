9d66dc6e2e170e626990c76c86820eb9637cdd11
charlieaco <carlosaco@gmail.com>
2026-05-01 11:07:17 -0300

    Add opentemp client

--- src/infrastructure/http/OpenMeteoAdapter.cpp
+++ src/infrastructure/http/OpenMeteoAdapter.cpp
@@ -8,16 +8,16 @@ bool OpenMeteoAdapter::getAuthToken(String& token) {
     return true;
 }
 
-bool OpenMeteoAdapter::getCurrentTemperature(const String&, const String&, double& temperature) {
+bool OpenMeteoAdapter::fetchResponse(String& response) {
     std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
     client->setInsecure();
-    Serial.println("OpenMeteo: getting temperature");
 
     HTTPClient https;
     String url = String("https://") + OPEN_METEO_HOST +
                  "/v1/forecast?latitude=" + LOCATION_LATITUDE +
                  "&longitude=" + LOCATION_LONGITUDE +
-                 "&current=temperature_2m";
+                 "&current=temperature_2m"
+                 "&timezone=America%2FArgentina%2FBuenos_Aires";
 
     if (!https.begin(*client, url)) {
         Serial.println("OpenMeteo: connection failed");
@@ -31,8 +31,37 @@ bool OpenMeteoAdapter::getCurrentTemperature(const String&, const String&, doubl
         return false;
     }
 
-    String response = https.getString();
+    response = https.getString();
     https.end();
+    return true;
+}
+
+bool OpenMeteoAdapter::getCurrentTime(String& currentTime) {
+    if (!fetchResponse(_cachedResponse)) {
+        _cachedResponse = "";
+        return false;
+    }
+
+    if (!JsonParser::extractOpenMeteoTime(_cachedResponse, currentTime)) {
+        Serial.println("OpenMeteo: failed to parse time");
+        _cachedResponse = "";
+        return false;
+    }
+
+    Serial.printf("Time obtained from Open-Meteo: %s\n", currentTime.c_str());
+    return true;
+}
+
+bool OpenMeteoAdapter::getCurrentTemperature(const String&, const String&, double& temperature) {
+    Serial.println("OpenMeteo: getting temperature");
+
+    String response;
+    if (_cachedResponse.length() > 0) {
+        response = _cachedResponse;
+        _cachedResponse = "";
+    } else {
+        if (!fetchResponse(response)) return false;
+    }
 
     if (!JsonParser::extractOpenMeteoTemperature(response, temperature)) {
         Serial.println("OpenMeteo: failed to parse temperature");
